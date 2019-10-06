7e1c380d06284ed7890ae6e7b936cd701ce2c99c
yiranqingqing <414379814@qq.com>
2019-10-06 22:52:23 +0800

    增加logout功能

--- core/src/main/java/com/share/handler/AuthenticationLogoutHandler.java
+++ core/src/main/java/com/share/handler/AuthenticationLogoutHandler.java
@@ -1,20 +1,37 @@
 package com.share.handler;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class AuthenticationLogoutHandler implements LogoutHandler {
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
+        try {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie != null) {
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+            response.getWriter().println("{\"code\":\"0\",\"message\": \" Logout successful. \"}");
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                //因为已经登出,不必再理会服务器内的错误,直接返回成功
+                response.getWriter().println("{\"code\":\"0\",\"message\": \" Logout successful. \"}");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
         return;
     }
 }
