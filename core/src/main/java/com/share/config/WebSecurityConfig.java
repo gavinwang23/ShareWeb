437777d07001a8531ca70c6d86934ba87cb79f9b
bikaiqiao <48797189+bikaiqiao@users.noreply.github.com>
2019-10-24 16:57:44 +0800

    完善is_username_duplicated接口

--- core/src/main/java/com/share/config/WebSecurityConfig.java
+++ core/src/main/java/com/share/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig {
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and().csrf().disable()
                     .authorizeRequests()
-                    .antMatchers("/static/**", "/index.jsp", "/api/sign_up", "/api/logout", "/api/is_username_duplicated", "/api/test")
+                    .antMatchers("/static/**", "/index.jsp", "/api/sign_up", "/api/logout", "/api/is_username_duplicated/**", "/api/test")
                     .permitAll()
                     .antMatchers(HttpMethod.OPTIONS)
                     .permitAll()
