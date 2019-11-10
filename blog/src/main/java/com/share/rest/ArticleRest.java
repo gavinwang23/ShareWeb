40c845c0e6e382df32fc61c85f2aa282eee74f9f
yiranqingqing <414379814@qq.com>
2019-11-10 22:46:51 +0800

    增加上传文件的功能

--- blog/src/main/java/com/share/rest/ArticleRest.java
+++ blog/src/main/java/com/share/rest/ArticleRest.java
@@ -1,16 +1,17 @@
 package com.share.rest;
 
 import com.mysql.cj.util.StringUtils;
-import com.share.entity.ArticleImageRequest;
+import com.share.common.CommonEnum;
 import com.share.entity.ArticleStation;
 import com.share.entity.BaseJsonResponse;
 import com.share.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.annotation.MultipartConfig;
+import java.io.*;
 import java.util.Date;
 
 @RestController
@@ -18,18 +19,21 @@ import java.util.Date;
 @ResponseBody
 public class ArticleRest extends BaseController {
 
+    @Value("${system.static.files.path}")
+    private String filePath;
+
     @Autowired
     private ArticleService articleService;
 
     @GetMapping("test")
-    public ArticleStation test(){
+    public ArticleStation test() {
         return articleService.selectByPrimaryKey(1L);
     }
 
     @PostMapping("article/add")
     public BaseJsonResponse addArticle(ArticleStation article) {
         if (article == null || StringUtils.isNullOrEmpty(article.getArticleTitle())
-                || StringUtils.isNullOrEmpty(article.getUserName()) )
+                || StringUtils.isNullOrEmpty(article.getUserName()))
             throw new RuntimeException("Can't input empty content.");
 
         if (article.getArticlePublishTime() == null)
@@ -37,18 +41,38 @@ public class ArticleRest extends BaseController {
 
         Integer result = articleService.insertSelective(article);
         if (result != 0)
-            throw new RuntimeException("Server internal error, please contact our engineer.");
+            throw new RuntimeException(CommonEnum.SERVER_INTERNAL_ERROR.getMessage());
 
         return new BaseJsonResponse();
     }
 
     @PostMapping(value = "/article/image/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public BaseJsonResponse addImageInArticle(
-            @RequestPart("files") MultipartFile[] files,
+            @RequestParam("files") MultipartFile[] files,
             @RequestParam(value = "userName", required = false) String userName,
             @RequestParam(value = "title", required = false) String title
-    ) {
-        MultipartFile file = files[0];
+    ) throws IOException {
+        if (files == null || files.length == 0 || StringUtils.isNullOrEmpty(userName) || StringUtils.isNullOrEmpty(title))
+            throw new RuntimeException(CommonEnum.NO_VERIFY_CODE.getMessage());
+
+        for (MultipartFile file : files) {
+            String path = filePath + "/" + file.getOriginalFilename();
+            File f = new File(path);
+            if (StringUtils.isNullOrEmpty(f.getParent()))
+                f.mkdirs();
+
+            f.createNewFile();
+            InputStream is = file.getInputStream();
+            FileOutputStream fos = new FileOutputStream(f);
+            byte[] b = new byte[1024];
+            int length;
+            while ((length = is.read(b)) > 0)
+                fos.write(b);
+
+            is.close();
+            fos.flush();
+            fos.close();
+        }
         return new BaseJsonResponse();
     }
 }
--- core/src/main/java/com/share/common/CommonEnum.java
+++ core/src/main/java/com/share/common/CommonEnum.java
@@ -7,7 +7,9 @@ public enum CommonEnum {
 
 
     //一些常用警告
-    NO_VERIFY_CODE(-1, "Please input correct verify-code.")
+    NO_VERIFY_CODE(-1, "Please input correct verify-code."),
+    NO_CORRECT_INPUT(-1, "Please input the correct parameter."),
+    SERVER_INTERNAL_ERROR(-1, "Server internal error, please contact our engineer.")
     ;
 
     CommonEnum(Integer code, String message) {
